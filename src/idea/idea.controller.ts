7d73ebd30c5cb3e161f32d65924004898a71aa5d
fdevos1 <felipe.devosm@gmail.com>
2023-08-30 21:03:03 -0300

    ideas controllers and services not tested yet

--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { IdeaService } from './idea.service';
+import { IdeaDTO } from './idea.dto';
 
 @Controller('idea')
-export class IdeaController {}
+export class IdeaController {
+  constructor(private ideaService: IdeaService) {}
+
+  @Get()
+  showAllIdeas() {
+    return this.ideaService.showAll();
+  }
+
+  @Get(':id')
+  readIdea(@Param('id') id: string) {
+    return this.ideaService.read(id);
+  }
+
+  @Post()
+  createIdea(@Body() data: IdeaDTO) {
+    return this.ideaService.create(data);
+  }
+
+  @Put(':id')
+  updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDTO>) {
+    return this.ideaService.update(id, data);
+  }
+
+  @Delete(':id')
+  deleteIdea(@Param('id') id: string) {
+    return this.ideaService.delete(id);
+  }
+}
